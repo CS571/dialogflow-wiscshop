c4a76f1d67c48d99730e2a3103ef029319528cab
Ilkyu Ju <ilkyu@cs.wisc.edu>
2021-11-30 20:01:30 -0600

    Initial commit

---
+++ webhook/webhook.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { WebhookClient } = require("dialogflow-fulfillment");
+const app = express();
+const fetch = require("node-fetch");
+const base64 = require("base-64");
+
+let username = "";
+let password = "";
+let token = "";
+
+USE_LOCAL_ENDPOINT = false;
+// set this flag to true if you want to use a local endpoint
+// set this flag to false if you want to use the online endpoint
+ENDPOINT_URL = "";
+if (USE_LOCAL_ENDPOINT) {
+  ENDPOINT_URL = "http://127.0.0.1:5000";
+} else {
+  ENDPOINT_URL = "http://cs571.cs.wisc.edu:5000";
+}
+
+async function getToken() {
+  let request = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + base64.encode(username + ":" + password),
+    },
+  };
+
+  const serverReturn = await fetch(ENDPOINT_URL + "/login", request);
+  const serverResponse = await serverReturn.json();
+  token = serverResponse.token;
+
+  return token;
+}
+
+app.get("/", (req, res) => res.send("online"));
+app.post("/", express.json(), (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res });
+
+  function welcome() {
+    agent.add("Webhook works!");
+    console.log(ENDPOINT_URL);
+  }
+
+  async function login() {
+    // You need to set this from `username` entity that you declare in DialogFlow
+    username = null;
+    // You need to set this from password entity that you declare in DialogFlow
+    password = null;
+    await getToken();
+
+    agent.add(token);
+  }
+
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  // You will need to declare this `Login` intent in DialogFlow to make this work
+  intentMap.set("Login", login);
+  agent.handleRequest(intentMap);
+});
+
+app.listen(process.env.PORT || 8080);
